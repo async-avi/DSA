ed8a9598ad163de69b0e74e1a45730b9f2e2298d
avi <avi.contact22@gmail.com>
2024-03-18 20:25:23 +0530

    Made a grading system using cpp

--- Step 1/1.1/basic.cpp
+++ Step 1/1.1/basic.cpp
@@ -1,11 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
+void getGrades(){
+    int marks;
+    string message = "Your grade is: ";
+    cin >> marks;
+    if(marks < 25){
+        cout << message << "F";
+    } else if (marks < 45){
+        cout << message << "E";
+    } else if (marks < 50){
+        cout << message << "D";
+    } else if (marks < 60){
+        cout << message << "C";
+    } else if (marks < 80){
+        cout << message << "B";
+    } else if (marks < 100) {
+        cout << message << "A";
+    } else {
+        cout << "Invalid number of marks";
+    }
+}
+
+
 int main (){
-    int x = 12;
-    int y;
-    cin >> y;
-    cout << "Hey " << x+y;
+    getGrades();
     return 0;
 }
