390e1d218b5437fcad56894493c12d6a28066d21
avi <avi.contact22@gmail.com>
2024-03-29 15:34:43 +0530

    Done vectors

--- Step 1/1.3 Cpp STL/index.cpp
+++ Step 1/1.3 Cpp STL/index.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 void printPairs(){
+    //normal pair
     pair<int, int> p = {1,2};
     cout << p.first << endl;
     //nested pairs
@@ -13,11 +14,65 @@ void printPairs(){
 }
 
 
+void printVectors(){
+    //single vector
+    vector <int> v;
+    v.push_back(1);
+
+    //pair vector
+    vector<pair<int, int> > vec;
+    vec.emplace_back(1,2);
+    vector<int>::iterator it = v.begin();
+    // cout << *it << endl;
+
+
+    //looping through vector data (pair)
+    // for(auto i: vec){
+    //     cout << i.first << endl;
+    // }
+
+    vector<int> random;
+    random.emplace_back(209);
+
+    //inserting
+    random.insert(random.begin(), 2,30);
+    // {30,30,209}
+
+    // random.erase(random.begin());
+    //{30,209}
+    // random.erase(random.end() -1);
+    //{30,30}
+
+    random.insert(random.begin(), 6, 100);
+
+    //erasing multiple
+    // random.erase(random.begin() + 2, random.end() -2);
+
+    //inserting vector in vector
+    // vector<int> copy(2, 50);
+    // random.insert(random.begin() + 1, copy.begin(), copy.end());
+
+
+
+    cout << "{";
+    for(auto p: random){
+        if(p == *(random.end()-1)){
+            cout << p;
+        }else{
+            cout << p << ",";
+        }
+        
+    }
+    cout << "}" << endl;
+
+
+}
+
 
 
 int main(){
    
-    printPairs();
-    //nested pairs
+    // printPairs();
+    printVectors();
     return 0;
 }
