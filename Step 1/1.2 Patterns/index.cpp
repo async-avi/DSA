21fe7426b38ff07ed546918990cc1e86ca9af032
Abhinav <abhinav.contact22@gmail.com>
2024-04-06 14:47:01 +0530

    Done hourglass and diamond

--- Step 1/1.2 Patterns/index.cpp
+++ Step 1/1.2 Patterns/index.cpp
@@ -117,9 +117,70 @@ void flippedHalfDiamond(int n){
     }
 }
 
+
+void diamond(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < 2*i + 1; j++){
+            cout << "*";
+        }
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < n; i ++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < 2*n - (2*i + 1); j++){
+            cout << "*";
+        }
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void hourglass(int n){
+    for(int i = 0; i < n; i ++){
+      for(int j = 0; j < i; j++){
+        cout << " ";
+      }
+      for(int j = 0; j < 2*n - (2*i + 1); j++){
+        cout << "*";
+      }
+      cout << endl;
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+
+        for(int j = 0; j < 2*i + 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void numberPyramid(int n){
+    for (int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main (){
     int n;
     cin >> n;
-    flippedHalfDiamond(n);
+    numberPyramid(n);
     return 0;
 }
