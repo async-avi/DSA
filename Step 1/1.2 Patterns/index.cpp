48088a139b4f10cb07e6bcf84f45fc4aba8d4933
Abhinav <abhinav.contact22@gmail.com>
2024-04-06 14:25:43 +0530

    Done all triangles

--- Step 1/1.2 Patterns/index.cpp
+++ Step 1/1.2 Patterns/index.cpp
@@ -46,8 +46,80 @@ void flippedInvertedPyramid(int n){
 }
 
 
+//Triangle
+
+void triangle(int n){
+    for(int i = 0; i < n; i ++){
+
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+
+        for(int j = 0 ; j < 2*i + 1; j++){
+            cout << "*";
+        }
+
+         for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+void invertedTriangle(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j ++){
+        cout << " ";
+       }
+        for(int j = 0; j < 2*n - (2*i + 1); j++){
+            cout << "*";
+        }
+
+       cout << endl;
+    }
+}
+
+void halfDiamond(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void flippedHalfDiamond(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+        for(int j = 0; j <= i; j ++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j <= i; j ++){
+            cout << " ";
+        }
+        for(int j = 0; j < n - i - 1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
 int main (){
     int n;
     cin >> n;
-    flippedInvertedPyramid(n);
+    flippedHalfDiamond(n);
+    return 0;
 }
