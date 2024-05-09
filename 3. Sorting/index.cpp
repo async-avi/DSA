66071209c627f924d0649ec00c9452504c9b0ab9
Abhinav <abhinav.contact22@gmail.com>
2024-05-09 19:06:36 +0530

    Done quick sort

--- 3. Sorting/index.cpp
+++ 3. Sorting/index.cpp
@@ -1,19 +1,49 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 using namespace std;
 
+int par(vector<int> &arr, int low, int high){
+    int p = arr[low];
+    int i = low;
+    int j = high;
 
-string largestNumber(vector<int>& nums) {
-        string final = "";
-        for(int i = nums.size() - 1; i >= 0; i--){
-            final+=to_string(nums[i]);
+    while(i < j){
+        while(arr[i] <= arr[p] && i <= high){
+            i++;
         }
 
-        return final;
+        while(arr[j] > arr[p] && j >= low){
+            j--;
+        }
+
+        if( i < j) swap(arr[i], arr[j]);
+    }
+
+    swap(arr[p], arr[j]);
+    return j;
+
 }
+
+void qs(vector<int> &arr, int low, int high){
+    if(low < high){
+        int pa = par(arr, low, high);
+
+        qs(arr, low, pa - 1);
+        qs(arr, pa + 1, high);
+    }
+}
+
+vector<int> quickSort(vector<int> arr)
+{
+    // Write your code here.
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+
 int main(){
-    vector<int> nums = {10,2};
-    // int arr[] = {3,3,2,2,2,1};
-    string res = largestNumber(nums);
-    cout << res << endl;
+    vector<int> arr = {5,6,7,1,2};
+    vector<int> res = quickSort(arr);
+
+    for(int i = 0; i < res.size(); i++) cout << res[i] << " ";
     return 0;
 }
