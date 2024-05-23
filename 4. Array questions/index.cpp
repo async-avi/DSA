6eeed569a7ca690b2460a727476b7896f51c52ad
Abhinav <abhinav.contact22@gmail.com>
2024-05-23 19:15:52 +0530

    Day 2

--- 4. Array questions/index.cpp
+++ 4. Array questions/index.cpp
@@ -2,21 +2,56 @@
 using namespace std;
 
 
- int reverse(int x) {
-    int p = 0;
+//max, min
+vector<int> maxMin(int arr[], int n){
+    int maxi = INT_MIN;
+    int mini = INT_MAX;
 
-    while(x > 0){
-        int last = x%10;
-        x = x/10;
-        p = (p*10) + last;
+    for(int i = 1; i < n; i++){
+        maxi = max(maxi, arr[i]);
+        mini = min(mini, arr[i]);
     }
 
-    return p;
+    return {maxi, mini};
+}
+
+
+//linear search
+int linearSearch(int arr[], int n, int k){ 
+    for(int i = 0; i < n; i++){
+        if(arr[i] == k){ 
+            return i;
+            break;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(int arr[], int n, int k){
+    int start = 0;
+    int end = n -1;
+
+    while(start<=end){
+        int mid = (start+end)/2;
+        if(arr[mid] == k){
+            return mid;
+        } else if(arr[mid] < k){
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
 }
 
 int main(){
-
     int n;
-    cin>>n;
-    cout<<reverse(n)<<endl;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << binarySearch(arr, n, 20) << endl;
+    return 0;
 }
