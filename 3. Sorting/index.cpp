858d024d720b2dd4cf574890317659034f12f51d
Abhinav <abhinav.contact22@gmail.com>
2024-05-24 19:03:45 +0530

    Day 2

--- 3. Sorting/index.cpp
+++ 3. Sorting/index.cpp
@@ -31,6 +31,79 @@ void bubbleSort(int arr[], int n){
 }
 
 
+//insertion sort 
+
+void iSort(int arr[], int n){
+ for(int i = 1; i < n; i++){
+	int current = arr[i];
+	int j = i - 1;
+
+	while(arr[j] > current && j >= 0){
+		arr[j + 1] = arr[j];
+        j--;
+	}
+
+	arr[j+1] = current;
+ }
+}
+
+
+//questions
+
+void printMaxTill(int arr[], int n){
+	int mx = INT_MIN;
+	for(int i = 0; i < n; i++){
+		mx = max(mx, arr[i]);
+		cout << mx << " ";
+	}
+}
+
+void sumOfSub(int arr[], int n){
+	int curr = 0;
+	for(int i = 0; i < n; i++){
+		curr = 0;
+		for(int j = i; j < n; j++){
+			curr += arr[j];
+			cout << curr << " ";
+		}
+
+	}
+}
+
+void recordBreak(int arr[], int n){
+
+	int ans = 0;
+	int mx = -1;
+
+	for(int i = 0; i < n; i++){
+		if(arr[i] > mx && arr[i] > arr[i+1]){
+			ans++;
+		}
+		mx = max(mx, arr[i]);
+	}
+
+	cout << ans << endl;
+}
+
+void repeating(int arr[], int n){
+	int index = INT_MAX;
+
+	if(n > 1){
+		for(int i = 0; i < n - 1; i++){
+		int curr = arr[i];
+		for(int j = i + 1; j < n; j++){
+			if(arr[j] == curr && i < index){
+				index = i;
+				break;
+			}
+		}
+	}
+	if(n == 1 || index == INT_MAX) cout << 0;
+	else cout << index;
+	
+	}
+}
+
 int main(){
 	int n;
 	cin >> n;
@@ -38,7 +111,7 @@ int main(){
 
 	for(int i = 0; i < n; i++) cin >> arr[i];
 
-	bubbleSort(arr, n);
-	for(int i = 0; i < n; i++) cout << arr[i] << " ";
+	repeating(arr, n);
+	// for(int i = 0; i < n; i++) cout << arr[i] << " ";
     return 0;
 }
