cefa9820d7a3f3c9a98a281619172c6f7ef43c5c
Abhinav <abhinav.contact22@gmail.com>
2024-05-27 13:44:28 +0530

    Done all square patterns

--- 0.1 Patterns/index.cpp
+++ 0.1 Patterns/index.cpp
@@ -14,13 +14,38 @@ void squarePatternStar(int n){
 
 
 void sqaurePatternNum(int n){
-
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j < n; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
 }
 
+
+void incrementNumSquare(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 1; j <= n; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void incrementNumInRow(int n){
+    int p = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            p+=1;
+            cout << p << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
     int n;
     cin >> n;
-
+    incrementNumInRow(n);
     return 0;
 }
