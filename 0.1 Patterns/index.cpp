4458e029805df9440377d3c1a1969abdb7c0020a
Abhinav <abhinav.contact22@gmail.com>
2024-05-27 13:33:21 +0530

    Done square pattern star

---
+++ 0.1 Patterns/index.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void squarePatternStar(int n){
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+}
+
+
+void sqaurePatternNum(int n){
+
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    return 0;
+}
