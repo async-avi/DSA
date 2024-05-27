6cd24e18c2e202d079b8427c4acdbe82316b58ae
Abhinav <abhinav.contact22@gmail.com>
2024-05-27 14:14:06 +0530

    Done triangular num pattern

--- 0.1 Patterns/index.cpp
+++ 0.1 Patterns/index.cpp
@@ -43,9 +43,60 @@ void incrementNumInRow(int n){
     }
 }
 
+void normalStarTriangle(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void normalNumPattern(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+void incrementRowTriangle(int n){
+    int p = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            p+=1;
+            cout << p << " ";
+        }
+        cout << endl;
+    }
+}
+
+void incrementRowWithNum(int n){
+    for(int i = 1; i <= n; i++){
+        int val = i;
+        for(int j = 1; j<=i; j++){
+            cout << val << " ";
+            val+=1;
+        }
+        cout << endl;
+    }
+    //think without extra variables
+}
+
+void reverseNumTriangle(int n){
+    for(int i = n - 1; i >= 0; i--){
+        for(int j = n - i; j > 0; j--){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+
 int main(){
     int n;
     cin >> n;
-    incrementNumInRow(n);
+    reverseNumTriangle(n);
     return 0;
 }
